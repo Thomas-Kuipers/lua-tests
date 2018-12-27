1db32a0b1ba0cd94ac9fa3eb55b944186615bc58
Thomas Kuipers <thomas@webvalid.nl>
2018-12-27 09:19:41 +0800

    travis config

--- specs/exampleFilter_spec.lua
+++
@@ -1,7 +0,0 @@
-local exampleFilter = require '../scripts/exampleFilter'
-
-describe('exampleFilter', function()
-  it('adds numbers', function()
-    assert.equal(5, exampleFilter.add(2,3))
-  end)
-end)
